ff9617e551faaae33f28f7b7639de1dfe7351b07
DarkkneZPanda <ortegaluis1114@gmail.com>
2023-06-14 14:13:48 -0700

    Added Player Input and Animations

--- Beginner-Games/Assets/PlayerInput.cs
+++ Beginner-Games/Assets/PlayerInput.cs
@@ -5,18 +5,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
+    public float moveSpeed = 150f;
+    Vector2 movementInput;
+    Rigidbody2D rb;
 
-    public Rigidbody2D rb;
-    public float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+      rb = GetComponent<Rigidbody2D>();  
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void FixedUpdate() {
+
+    // # 1
+    // rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+
+    // # 2 works with Linear Drag
+    // if(movementInput.x != || movementInput.y != 0) {
+       // rb.velocity = movementInput * moveSpeed;
+    // }
+
+    // # 3 works with Linear Drag
+     rb.AddForce(movementInput* moveSpeed);
+   }
+
+   void OnMove(InputValue movementValue) {
+        movementInput = movementValue.Get<Vector2>();
+   }
 }
