01bb67d08f1d5d14e76dafa19cd4a181e684a7a3
DarkkneZPanda <ortegaluis1114@gmail.com>
2023-07-21 00:34:17 -0700

    Better Weapon rotation

--- Beginner-Games/Assets/Weapon.cs
+++ Beginner-Games/Assets/Weapon.cs
@@ -6,26 +6,33 @@ using UnityEngine.InputSystem;
 public class Weapon : MonoBehaviour
 {
     private Transform aimWeapon;
+    private Animator animator;
     // public GameObject bullet;
     // public float fireForce = 20f;
-    Vector3 mousePos;
-    Vector3 worldPos;
 
     private void Awake() {
         aimWeapon = transform.Find("Aim");
+        animator = aimWeapon.GetComponent<Animator>();
     }
 
     private void Update() {
-        mousePos = Mouse.current.position.ReadValue(); // Reads local mouse Postion
-        worldPos = Camera.main.ScreenToWorldPoint(mousePos); // converts local to worldspace mouse position
-
-        Vector3 aimDirect = (mousePos - transform.position).normalized;
-        float angle = Mathf.Atan2(aimDirect.y, aimDirect.x) * Mathf.Rad2Deg;
-        aimWeapon.eulerAngles = new Vector3(0, 0, angle);
+        Aiming();
+        Shooting();
         
     }
 
-    public void Fire() {
-        
+    private void Aiming() {
+        Vector3 mousePos = Mouse.current.position.ReadValue(); // Reads local mouse Postion
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos); // converts local to worldspace mouse position
+
+        Vector3 aimDirect = (worldPos - transform.position).normalized; // 
+        float angle = Mathf.Atan2(aimDirect.y, aimDirect.x) * Mathf.Rad2Deg; // Handles rotation
+        aimWeapon.eulerAngles = new Vector3(0, 0, angle); //
+    }
+
+    private void Shooting() {
+        if(Mouse.current.leftButton.wasPressedThisFrame) {
+            animator.SetTrigger("Shoot");
+        }
     }
 }
